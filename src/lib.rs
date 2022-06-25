a1fb1f3dea1f3998b29ce2428956d561f2c0b67d
Alexey Yerin <yyp@disroot.org>
2022-06-25 22:51:41 +0300

    Replace Align{4,8,16} with one instruction
    This makes it easier for frontends to give it the correct alignment.

--- src/lib.rs
+++ src/lib.rs
@@ -58,12 +58,8 @@ pub enum Instr<'a> {
     Jmp(String),
     /// Calls a function
     Call(String, Vec<(Type<'a>, Value)>),
-    /// Allocates a 4-byte aligned area on the stack
-    Alloc4(u32),
-    /// Allocates a 8-byte aligned area on the stack
-    Alloc8(u64),
-    /// Allocates a 16-byte aligned area on the stack
-    Alloc16(u128),
+    /// Allocates an area on the stack
+    Alloc(u64, usize),
     /// Stores a value into memory pointed to by destination.
     /// `(type, destination, value)`
     Store(Type<'a>, Value, Value),
@@ -124,9 +120,13 @@ impl<'a> fmt::Display for Instr<'a> {
                         .join(", "),
                 )
             }
-            Self::Alloc4(size) => write!(f, "alloc4 {}", size),
-            Self::Alloc8(size) => write!(f, "alloc8 {}", size),
-            Self::Alloc16(size) => write!(f, "alloc16 {}", size),
+            Self::Alloc(size, align) => {
+                assert!(
+                    *align == 4 || *align == 8 || *align == 16,
+                    "Invalid stack alignment"
+                );
+                write!(f, "alloc{} {}", align, size)
+            }
             Self::Store(ty, dest, value) => {
                 if matches!(ty, Type::Aggregate(_)) {
                     unimplemented!("Store to an aggregate type");
